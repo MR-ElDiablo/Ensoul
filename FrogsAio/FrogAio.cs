7e9f538e579a5d05e23e0cbbcafa8839c2c4c48a
MrDiablo <70061730+MR-ElDiablo@users.noreply.github.com>
2020-09-25 15:16:52 +0200

    Add files via upload

---
+++ FrogsAio/FrogAio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EnsoulSharp.SDK;
+using Froggy;
+namespace ForggyAio
+{
+    class FrogAio
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+        static void OnGameLoad()
+        {
+            try
+            {
+                switch (GameObjects.Player.CharacterName)
+                {
+                    case "Thresh":
+                        Thresh.OnGameLoad();
+                        break;
+                    case "BlitzCrank":
+                        FrogBlitzcrank.OnGameLoad();
+                        break;
+                    case "Riven":
+                        Riven.OnGameLoad();
+                        break;
+                }
+                SkinChanger.OnLoad();
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+        }
+    }
+}
