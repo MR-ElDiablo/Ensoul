1d87a2f526e4eec1bcc591a74bafcb380b40bc08
MrDiablo <70061730+MR-ElDiablo@users.noreply.github.com>
2020-09-25 15:17:16 +0200

    Add files via upload

---
+++ FrogsAio/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FroggyAio")]
+[assembly: AssemblyDescription("Used With Ensoul Loader")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Frogs")]
+[assembly: AssemblyProduct("Diablo")]
+[assembly: AssemblyCopyright("@Diablo")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ebde7f6-4fd1-4bc7-b00a-aa0129c9dcc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
