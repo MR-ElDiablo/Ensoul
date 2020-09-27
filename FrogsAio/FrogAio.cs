8a2a1b9d6fe39ef8131dcb2e843e5299d90ea83f
MrDiablo <70061730+MR-ElDiablo@users.noreply.github.com>
2020-09-27 19:21:47 +0200

    Add files via upload

---
+++ FrogsAio/FrogAio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using EnsoulSharp.SDK;
+using Froggy;
+using EnsoulSharp.SDK.MenuUI;
+using EnsoulSharp.SDK.MenuUI.Values;
+using FrogThresh;
+
+namespace ForggyAio
+{
+    class FrogAio
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+        static void OnGameLoad()
+        {
+            Menu menu;
+            try
+            {
+                menu = new Menu("FroggyAio", "FroggyAio",true);
+                menu.Add(new MenuSeparator("set", "Press F5 2x To Apply"));
+                menu.Add(new MenuBool("ChampionScripts", "Use Frogs Champ"));
+                menu.Add(new MenuBool("SkinChanger", "Use Frogs SkinChanger"));
+                menu.Attach();
+                if (menu["ChampionScripts"].GetValue<MenuBool>().Enabled) switch (GameObjects.Player.CharacterName)
+                {
+                    case "Thresh":
+                        Program.OnGameLoad();
+                        break;
+                    case "BlitzCrank":
+                        FrogBlitzcrank.OnGameLoad();
+                        break;
+                    case "Riven":
+                        Riven.OnGameLoad();
+                        break;
+
+                }
+
+                if(menu["SkinChanger"].GetValue<MenuBool>().Enabled) SkinChangers.OnLoad();
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+        }
+    }
+}
